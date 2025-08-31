03d6757ddca1a50ab5250eaaa2b6d76ed9b14651
Yahya <yahyashafiq309@gmail.com>
2025-08-31 12:35:16 +0500

    Null operator

---
+++ nullishOperator.js
@@ -0,0 +1,15 @@
+// Nullish Coalescing Operator (??)
+
+let val1 = 10 ?? 20
+console.log(val1)
+
+let val2 = null ?? 12;
+console.log(val2)
+
+let val3 = undefined ?? 16
+console.log(val3);
+
+
+let val4 = null ?? 20 ?? 30
+console.log(val4);
+
