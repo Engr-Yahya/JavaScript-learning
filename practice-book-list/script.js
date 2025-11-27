5a13663ccae8ef306fa8b59ad1e6eb976026b8b9
Yahya <yahyashafiq309@gmail.com>
2025-11-27 16:42:48 +0500

    restarting

--- practice-book-list/script.js
+++
@@ -1,7 +0,0 @@
-const list = document.querySelector("#book-list ul");
-list.addEventListener("click", (e) => {
-  if (e.target.className == "delete") {
-    const li = e.target.parentElement;
-    list.removeChild(li);
-  }
-});
